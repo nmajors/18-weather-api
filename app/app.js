fa875d854473d5d88f5c9f88b5ae0c081f31a30b
Nancy Majors <nmajors@gmail.com>
2016-04-03 18:07:49 -0500

    changed api call, adjusted javascript to create the elements and put them all on page, minor style tweaks.

--- app/app.js
+++ app/app.js
@@ -13,36 +13,32 @@ navigator.geolocation.getCurrentPosition(function(position) {
       return response.json();
     })
     .then((response) => {
-      // console.log(response);
       document.querySelector("#cityName").textContent = response.name;
       document.querySelector("#currentCondition").textContent = response.weather[0].description;
       let currentTemp = Math.round(Number(response.main.temp));
       document.querySelector("#currentTemp").textContent = `${currentTemp}°`;
     });
 
-  fetch(`${baseUrl}forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
+  fetch(`${baseUrl}forecast/daily?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`)
     .then((response) => {
       return response.json();
     })
     .then((response) => {
       console.log(response);
+      console.log(response.list);
 
-      let day1 = response.list[2];
-      let day2 = response.list[10];
-      let day3 = response.list[18];
-      let day4 = response.list[26];
-      let day5 = response.list[34];
+      let days = response.list;
+      var day;
+      days.forEach((day) => {
+        let date = new Date(day.dt * 1000);
+        let dayOfWeekValue = date.getDay();
 
+        let highTempValue = Math.round(Number(day.temp.max));
+        let lowTempValue = Math.round(Number(day.temp.min));
+        let iconCode = day.weather[0].icon;
+        let iconCodeUrl = `http://openweathermap.org/img/w/${iconCode}.png`
 
-      let date1 = new Date(day1.dt * 1000);
-      let dayOfWeekValue = date1.getDay();
-
-      let highTempValue = Math.round(Number(day1.main.temp));
-      let lowTempValue = Math.round(Number(response.list[0].main.temp));
-      let iconCode = day1.weather[0].icon;
-      let iconCodeUrl =`http://openweathermap.org/img/w/${iconCode}.png`
-
-      var dayOfWeekName
+        var dayOfWeekName
         switch (dayOfWeekValue) {
           case 0:
             dayOfWeekName = "Monday";
@@ -69,50 +65,31 @@ navigator.geolocation.getCurrentPosition(function(position) {
             dayOfWeekName = "Error";
             break;
         }
+        let forecast = document.querySelector(".forecast");
+        let row = document.createElement("div");
+        row.classList.add("row");
+        forecast.appendChild(row);
+        let dayOfWeek = document.createElement("div");
+        dayOfWeek.classList.add("dayOfWeek");
+        row.appendChild(dayOfWeek);
+        dayOfWeek.textContent = dayOfWeekName;
 
-        let day1Div = document.querySelector("#day1");
-          let dayOfWeek = document.createElement("div");
-            dayOfWeek.classList.add("dayOfWeek");
-            day1Div.appendChild(dayOfWeek);
-            dayOfWeek.textContent = dayOfWeekName;
+        let weatherIcon = document.createElement("div");
+        weatherIcon.classList.add("weatherIcon");
+        row.appendChild(weatherIcon);
+        let weatherIconImg = document.createElement("img");
+        weatherIconImg.src = iconCodeUrl;
+        weatherIcon.appendChild(weatherIconImg);
 
-          let weatherIcon = document.createElement("div");
-            weatherIcon.classList.add("weatherIcon");
-            day1Div.appendChild(weatherIcon);
-          let weatherIconImg = document.createElement("img");
-            weatherIconImg.src = iconCodeUrl;
-            weatherIcon.appendChild(weatherIconImg);
+        let highTemp = document.createElement("div");
+        highTemp.classList.add("highTemp");
+        row.appendChild(highTemp);
+        highTemp.textContent = `${highTempValue}°`;
 
-          let highTemp = document.createElement("div");
-            highTemp.classList.add("highTemp");
-            day1Div.appendChild(highTemp);
-            highTemp.textContent = `${highTempValue}°`;
-
-          let lowTemp = document.createElement("div");
-            lowTemp.classList.add("lowTemp");
-            day1Div.appendChild(lowTemp);
-            lowTemp.textContent = `${lowTempValue}°`;
-
-
-
-
-      //   console.log(dayOfWeekName);
-      // let rows = document.querySelectorAll(".row");
-      // rows.forEach((row) => {
-      //   let dayOfWeek = document.createElement("div");
-      //     dayOfWeek.classList.add("dayOfWeek");
-      //     day1.appendChild(dayOfWeek);
-      //     dayOfWeek.textContent = dayOfWeekName;
-      //
-      // })
+        let lowTemp = document.createElement("div");
+        lowTemp.classList.add("lowTemp");
+        row.appendChild(lowTemp);
+        lowTemp.textContent = `${lowTempValue}°`;
+      })
     });
 });
-
-
-// fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?lat=<latitude>&lon=<longitude>&APPID=<97e2a65458fa6ffa369e9f2c945bd316>&units=imperial`)
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((response) => {
-//     console.log(response);
-//   });
